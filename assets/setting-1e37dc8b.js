b2ec8c7ef8cb4a2350f4b2bf89b7936d6fbf504f
zxs <1907231867@qq.com>
2024-03-08 14:35:42 +0800

    update

---
+++ assets/setting-1e37dc8b.js
@@ -0,0 +1,1 @@
+import{bb as t}from"./index-537f057f.js";let e=t("SettingStore",{state:()=>({fold:!1,refsh:!1})});const o=e;export{o as u};
---
+++ assets/time-3c885e48.js
@@ -0,0 +1,1 @@
+const t=()=>{var s=new Date().getHours();let e="";return s<=9?e="早上":s<=12?e="中午":s<=18?e="下午":e="晚上",e};export{t as g};
